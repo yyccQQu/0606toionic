d8708ddcae569040f03c96f738f397491e771f9d
yyccQQu <643929860@qq.com>
2018-06-07 23:25:00 +0800

    toBuild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
+import { HomePage } from "../pages/home/home";
+import { DiscoveryPage } from "../pages/discovery/discovery";
+import { ChatPage } from "../pages/chat/chat";
+import { NotificationPage } from "../pages/notification/notification";
+import { MorePage } from "../pages/more/more";
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +17,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
     TabsPage
   ],
   imports: [
@@ -25,10 +30,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
+    MyApp,    
     HomePage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
     TabsPage
   ],
   providers: [
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { DiscoveryPage } from '../discovery/discovery';
+import { ChatPage } from '../chat/chat';
+import { NotificationPage } from '../notification/notification';
+import { MorePage } from '../more/more';
+
+
 
 @Component({
-  templateUrl: 'tabs.html'
+  templateUrl: "tabs.html"
 })
 export class TabsPage {
+  tabHome = HomePage;
+  tabDiscovery = DiscoveryPage;
+  tabChat = ChatPage;
+  tabNotification = NotificationPage;
+  tabMore = MorePage;
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
-
-  constructor() {
-
-  }
+  constructor() {}
 }
